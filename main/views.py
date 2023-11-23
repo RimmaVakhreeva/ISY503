423891afece3cd6bf82f0f3250e339207cd49826
Pavel Bryzgalov <pavelbryz@gmail.com>
2023-11-23 19:02:58 +1030

    Initial

---
+++ main/views.py
@@ -0,0 +1,6 @@
+from django.core.handlers.wsgi import WSGIRequest
+from django.shortcuts import render
+
+# Create your views here.
+def index(request: WSGIRequest):
+    return render(request, 'main/index.html')
